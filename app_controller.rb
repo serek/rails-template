95c4c4d4a83b72c1d9561dfb68532863da1a36fd
Zachery Hostens <zacheryph@gmail.com>
2009-11-12 23:24:56 -0600

    remove files i dont need right now (may be created later)

--- app_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  include NinjaHideout::ControllerMixin
-  
-  layout "application"
-  helper :all
-  protect_from_forgery
-  
-end
--- user_blueprint.rb
+++
@@ -1,8 +0,0 @@
-# Default User Blueprint
-
-User.blueprint do
-  login                 { InternetForgery.user_name }
-  display_name          { NameForgery.full_name }
-  password              "password"
-  password_confirmation "password"
-end
